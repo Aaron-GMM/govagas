c03bf6d2fce0f30c5da021bd2edb911753c05392
Aaron Gibran <aarongibran13@gmail.com>
2026-02-05 15:21:11 -0300

    feat(GORM): install GORM and initilize logger

--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
-import "github.com/Aaron-GMM/govagas/router"
+import (
+	"github.com/Aaron-GMM/govagas/config"
+	"github.com/Aaron-GMM/govagas/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+
+	logger = *config.GetLogger("main")
+
+	var err error
+	err = config.Init()
+
+	if err != nil {
+		logger.ErrorF("config init err: %v", err)
+		return
+	}
+
 	// incializando router
 	router.InitRouter()
 }
