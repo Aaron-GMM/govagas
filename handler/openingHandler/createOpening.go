f462826af827066f9655e668dd1de9b658361515
Aaron Gibran <aarongibran13@gmail.com>
2026-02-09 14:54:45 -0300

    feat(handler): add request

--- handler/openingHandler/createOpening.go
+++ handler/openingHandler/createOpening.go
@@ -1,13 +1,17 @@
 package openingHandler
 
 import (
-	"net/http"
-
+	"github.com/Aaron-GMM/govagas/config"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "CreateOpeningHandler",
-	})
+	request := struct {
+		role string
+	}{}
+
+	ctx.BindJSON(&request)
+
+	config.Logger.InforF("request received: %+v", request)
+
 }
