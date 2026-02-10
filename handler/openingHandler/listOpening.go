8aaf0316324154e25dcb2263d3871b488c2af834
Aaron Gibran <aarongibran13@gmail.com>
2026-02-10 14:29:46 -0300

    feat(listopening):create and exceptions for list openings

--- handler/openingHandler/listOpening.go
+++ handler/openingHandler/listOpening.go
@@ -3,11 +3,20 @@ package openingHandler
 import (
 	"net/http"
 
+	"github.com/Aaron-GMM/govagas/handler"
+	"github.com/Aaron-GMM/govagas/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ListOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "ListOpeningHandler",
-	})
+	openins := &[]schemas.Opening{}
+	if err := handler.Db.Find(openins).Error; err != nil {
+		handler.SendError(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+	if len(*openins) == 0 {
+		handler.SendError(ctx, http.StatusNotFound, "[]")
+		return
+	}
+	handler.SendSuccess(ctx, "list-openings", openins)
 }
