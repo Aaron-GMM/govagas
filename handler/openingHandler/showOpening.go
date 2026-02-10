0e1e0594006a1b4cdea5edcee89f384fd1344e43
Aaron Gibran <aarongibran13@gmail.com>
2026-02-10 14:37:00 -0300

    feat(showOpening):create and exceptions for show  openings

--- handler/openingHandler/showOpening.go
+++ handler/openingHandler/showOpening.go
@@ -3,11 +3,22 @@ package openingHandler
 import (
 	"net/http"
 
+	"github.com/Aaron-GMM/govagas/handler"
+	"github.com/Aaron-GMM/govagas/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "ShowOpeningHandler",
-	})
+	id := ctx.Query("id")
+
+	if id == "" {
+		handler.SendError(ctx, http.StatusBadRequest, handler.ErrParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+	opening := &schemas.Opening{}
+	if err := handler.Db.First(&opening, id).Error; err != nil {
+		handler.SendError(ctx, http.StatusNotFound, "opening not found")
+		return
+	}
+	handler.SendSuccess(ctx, "show-opening", *opening)
 }
