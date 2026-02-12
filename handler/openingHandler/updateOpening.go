48838b3dc0974a4813fd2e3e5c84263c4dc06d72
Aaron Gibran <aarongibran13@gmail.com>
2026-02-12 08:53:46 -0300

    feat(upadte):creating update opening logic

--- handler/openingHandler/updateOpening.go
+++ handler/openingHandler/updateOpening.go
@@ -3,11 +3,45 @@ package openingHandler
 import (
 	"net/http"
 
+	"github.com/Aaron-GMM/govagas/handler"
+	"github.com/Aaron-GMM/govagas/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func UpdateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "UpdateOpeningHandler",
-	})
+	request := &handler.UpadeteOpeningRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		handler.Logger.ErrorF("Validate request error: %v", err.Error())
+		handler.SendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id := ctx.Query("id")
+
+	if id == "" {
+		handler.Logger.ErrorF("Get opening id error: %v", id)
+		handler.SendError(ctx, http.StatusBadRequest, handler.ErrParamIsRequired("id",
+			"queryParameter").Error())
+		return
+	}
+
+	opening := &schemas.Opening{}
+
+	if err := handler.Db.First(&opening, id).Error; err != nil {
+		handler.Logger.ErrorF("Get opening error: %v", err.Error())
+		handler.SendError(ctx, http.StatusNotFound, "Opening not found")
+		return
+	}
+
+	if err := handler.Db.Model(&opening).Updates(*request).Error; err != nil {
+		handler.Logger.ErrorF("Update opening error: %v", err.Error())
+		handler.SendError(ctx, http.StatusBadRequest, "Update opening error")
+		return
+	}
+
+	handler.SendSuccess(ctx, "update-opening", *opening)
+
 }
