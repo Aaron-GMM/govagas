cf5d5af2e089a91a9f1e080e1123ad6e6a926547
Aaron Gibran <aarongibran13@gmail.com>
2026-03-05 19:19:31 -0300

    refactor(api): inject dependencies into handlers and restructure routes

--- router/routers.go
+++ router/routers.go
@@ -1,7 +1,6 @@
 package router
 
 import (
-	"github.com/Aaron-GMM/govagas/handler"
 	"github.com/Aaron-GMM/govagas/handler/openingHandler"
 	"github.com/gin-gonic/gin"
 )
@@ -12,20 +11,13 @@ que ele se localiza para acessar ele sendo bem mais eficiente
 em vez de acessar uma instacia do router para cada função acesso apenas um router sendo bem mais eficiente
 */
 
-func initializeRouter(router *gin.Engine) {
-	handler.InitializeHandler()
-
+func initializeRouter(router *gin.Engine, opHandler *openingHandler.OpeningHandler) {
 	v1 := router.Group("/api/v1")
 	{
-		// show Oportunity
-		v1.GET("/opening", openingHandler.ShowOpeningHandler)
-		// create Oportunity
-		v1.POST("/opening", openingHandler.CreateOpeningHandler)
-		// update Oportunity
-		v1.PUT("/opening", openingHandler.UpdateOpeningHandler)
-		// delete Oportunity
-		v1.DELETE("/opening", openingHandler.DeleteOpeningHandler)
-		// show all Oportunity
-		v1.GET("/openings", openingHandler.ListOpeningHandler)
+		v1.GET("/opening", opHandler.ShowOpeningHandler)
+		v1.POST("/opening", opHandler.CreateOpeningHandler)
+		v1.PUT("/opening", opHandler.UpdateOpeningHandler)
+		v1.DELETE("/opening", opHandler.DeleteOpeningHandler)
+		v1.GET("/openings", opHandler.ListOpeningHandler)
 	}
 }
