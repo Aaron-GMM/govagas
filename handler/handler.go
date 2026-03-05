27ed9eb40ae7f0aafc2d480f4fe81cacf4819966
Aaron Gibran <aarongibran13@gmail.com>
2026-03-05 11:52:26 -0300

    refactor: Implement the repository pattern and isolate data acess

--- handler/handler.go
+++ handler/handler.go
@@ -2,15 +2,12 @@ package handler
 
 import (
 	"github.com/Aaron-GMM/govagas/config"
-	"gorm.io/gorm"
 )
 
 var (
 	Logger *config.Logger
-	Db     *gorm.DB
 )
 
 func InitializeHandler() {
 	Logger = config.GetLogger("handler")
-	Db = config.GetDB()
 }
