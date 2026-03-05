f2b5e130b1b4fc5bea0ce322561fb6c38ce8cf40
Aaron Gibran <aarongibran13@gmail.com>
2026-03-05 19:18:26 -0300

    refactor(core): alter logic method delete and response for repository

--- service/openingService.go
+++ service/openingService.go
@@ -26,12 +26,12 @@ var logger = config.GetLogger("OpeningService")
 
 func (s *openingService) CreateOpening(openingRequest *schemas.Opening) (*schemas.Opening, error) {
 	logger.InforF("Create opening %v", openingRequest.Name)
-	opening, err := s.repository.CREATE(*openingRequest)
+	err := s.repository.Create(openingRequest)
 	if err != nil {
-		logger.ErrorF("Create opening %v error: %v", opening.ID, err)
+		logger.ErrorF("Create opening  error: %v", err)
 		return nil, err
 	}
-	return &opening, nil
+	return openingRequest, nil
 }
 
 func (s *openingService) GetOpening(id string) (*schemas.Opening, error) {
@@ -47,7 +47,7 @@ func (s *openingService) GetOpening(id string) (*schemas.Opening, error) {
 
 func (s *openingService) UpdateOpening(openingRequest *schemas.Opening) (*schemas.Opening, error) {
 	logger.InforF("Update opening %v", openingRequest.Name)
-	newOpening, err := s.repository.UPDATE(*openingRequest)
+	newOpening, err := s.repository.Update(*openingRequest)
 	if err != nil {
 		logger.ErrorF("Update opening %v error: %v", openingRequest.Name, err)
 		return nil, err
@@ -56,7 +56,12 @@ func (s *openingService) UpdateOpening(openingRequest *schemas.Opening) (*schema
 }
 func (s *openingService) DeleteOpening(id string) error {
 	logger.InforF("Delete opening %v", id)
-	err := s.repository.DELETE(id)
+	_, err := s.GetOpening(id)
+	if err != nil {
+		logger.ErrorF("Erro: Vaga %v não encontrada para exclusão", id)
+		return err
+	}
+	err = s.repository.Delete(id)
 	if err != nil {
 		logger.ErrorF("Delete opening %v error: %v", id, err)
 		return err
@@ -66,7 +71,7 @@ func (s *openingService) DeleteOpening(id string) error {
 
 func (s *openingService) ListOpenings() ([]*schemas.Opening, error) {
 	logger.InforF("List openings")
-	openings, err := s.repository.LIST()
+	openings, err := s.repository.List()
 	if err != nil {
 		logger.ErrorF("List openings error: %v", err)
 		return nil, err
